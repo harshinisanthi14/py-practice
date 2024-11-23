28e32285bd60ff27706b4ab3ad4a48e093a46ac4
harshinisanthi14 <harshiniudayagiri123@gmail.com>
2024-11-23 21:48:40 +0530

    jd

--- pra.py
+++ pra.py
@@ -1,13 +1,20 @@
 import math
 
 
-print(math.pi)
-print(math.e)
-x=2.7
-result = math.sqrt(x)
-result=math.ceil(x)
-result=math.floor(x)
+# print(math.pi)
+# print(math.e)
+# x=2.7
+# result = math.sqrt(x)
+# result=math.ceil(x)
+# result=math.floor(x)
 
-print(result)
+# print(result)
 
-print(math.sqrt(4))
+# print(math.sqrt(4))
+
+
+radius = int(input("enter the radius of a circle: "))
+
+circumference = 2 * math.pi * radius
+
+print(f"the circumference is {round(circumference,2)} ")
