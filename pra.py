ad108e984afa68d1abf491f1ed50fe539396009d
harshinisanthi14 <harshiniudayagiri123@gmail.com>
2024-11-23 22:01:34 +0530

    hd

--- pra.py
+++ pra.py
@@ -13,8 +13,29 @@ import math
 # print(math.sqrt(4))
 
 
-radius = int(input("enter the radius of a circle: "))
+# radius = int(input("enter the radius of a circle: "))
 
-circumference = 2 * math.pi * radius
+# circumference = 2 * math.pi * radius
 
-print(f"the circumference is {round(circumference,2)} ")
+# print(f"the circumference is {round(circumference,2)} ")
+
+# radius = int(input("enter radius: "))
+# area = math.pi * pow(radius,2)
+
+# print(f"the area is {round(area,2)}")
+
+
+a = int(input("enter a value: "))
+b=int(input("enter a value: "))
+
+# a=pow(a,2)
+# b=pow(b,2)
+
+# c=a+b
+
+# c=math.sqrt(c)
+
+# print(f"The value of c is {c}")
+
+c=math.sqrt(pow(a,2) + pow(b,2))
+print(round(c,2))
