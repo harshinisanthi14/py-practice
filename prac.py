2f3034bb2b1178b43de1edd01d81ef88e7a8a880
harshinisanthi14 <harshiniudayagiri123@gmail.com>
2024-11-23 20:28:21 +0530

    first commit

---
+++ prac.py
@@ -0,0 +1,53 @@
+#variables
+# age=21
+# print(f"your age is {age}")
+
+# name="harshi"
+# print(f"hello {name}")
+
+# marks = 30.5
+# print(f"your maarks are {marks}")
+
+# is_number=True
+# print(f"this is {is_number}")
+
+# is_name = True
+# if is_name:
+#     print("your name is h")
+# else:
+#     print("i don't know")    
+
+
+#type casting
+
+# name="harshini"
+# name1=""
+# age=21
+# marks=27.2
+# is_there=True
+
+
+# marks = int(marks)
+# print(marks)
+
+# age=float(age)
+# print(age)
+
+# name=bool(name)
+# print(name)
+
+# name1=bool(name1)
+# print(name1)
+
+#input
+# name=input("enter your name: ")
+# age=int(input("how old are you? "))
+# age=age+1
+# print(f"my name is {name}")
+# print("Happy Birthday to u")
+# print(f"I am {age} years old")
+
+length=float(input("enter the length: "))
+width=float(input("enter the width: "))
+area = length * width
+print(f"the area is {area}")
