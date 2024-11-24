b41b5d01dcb5a8239ce1add3bcd3790478544dd8
harshinisanthi14 <harshiniudayagiri123@gmail.com>
2024-11-24 20:14:30 +0530

    m

--- prac.py
+++ prac.py
@@ -344,27 +344,55 @@
 
 # print(f"your number is {num}")
 
+# principle = 0
+# rate = 0
+# time = 0
+
+# while principle <= 0:
+#     principle = float(input("enter your principle: "))
+#     if principle <= 0:
+#         print("principle amount can't be less than zero")
+
+# while rate <= 0:
+#     rate = float(input("enter your rate: "))
+#     if rate <= 0:
+#         print("rate should can't be zero")
+
+# while time <= 0:
+#     time = int(input("enter time: "))
+#     if time <= 0:
+#         print("time can't be zero")
+
+# total = principle * pow((1 + rate / 100), time)
+# print(f"balance after {time}years/s :Rs.{total:.2f}")        
+
 principle = 0
 rate = 0
 time = 0
 
-while principle <= 0:
+while True:
     principle = float(input("enter your principle: "))
-    if principle <= 0:
+    if principle < 0:
         print("principle amount can't be less than zero")
+    else:
+        break
 
-while rate <= 0:
+while True:
     rate = float(input("enter your rate: "))
-    if rate <= 0:
+    if rate < 0:
         print("rate should can't be zero")
+    else:
+        break
 
-while time <= 0:
+while True:
     time = int(input("enter time: "))
-    if time <= 0:
+    if time < 0:
         print("time can't be zero")
+    else:
+        break
 
 total = principle * pow((1 + rate / 100), time)
-print(f"balance after {time}years/s :Rs.{total:.2f}")        
+print(f"balance after {time}years/s :Rs.{total:.2f}")   
 
 
     
